a5fe9adad4882293ba3a46eccd42b583f0eb89e7
Olesya555 <olet.ru@gmail.com>
2022-09-10 07:40:14 +0600

    Повторяем за преподом. Делаем автотесты по CI/CD???

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;// Создаём класс для тестирования
+
+public class AnimalTest { // Делаем аннотацию @Test
+
+    @Test
+    // Чтобы написать тест делаем новый метод
+    public void shouldEat(){ // Название метода начинаем со слова should (шут) - должен, далее
+        // 
+
+    }
+}
